628b24b237cccf0b4ac0a2db8d0d83ea980c346c
precious123-gifted <ogunameprecious123@gmail.com>
2022-08-24 12:10:58 +0100

    august 24 12:10

--- src/components/main-display/side-bar/menulist/MenuList.jsx
+++ src/components/main-display/side-bar/menulist/MenuList.jsx
@@ -1,37 +1,48 @@
 import React, { Component } from 'react'
 import { Menulist } from './MenuList.styled'
 import { NavLink } from 'react-router-dom'
+import homecon from '../../../../assets/homecon.png'
+import programcon from '../../../../assets/programscon.png'
+import chatroomcon from '../../../../assets/chatroomcon.png'
+import calendacon from '../../../../assets/calendacon.png'
+import helpcon from '../../../../assets/helpcon.png'
+import settingscon from '../../../../assets/settingscon.png'
+
 
 export class MenuList extends Component {
   render() {
     return (
       <Menulist>
 <NavLink to='/' className="list-item">
-<div className='icon'></div> <div className="text">Home</div>
+  <div className="content">
+<div className='icon'><img src={homecon} alt="" /></div> <div className="text">Home</div></div>
 </NavLink>
 
 <NavLink to='/' className="list-item">
-<div className='icon'></div> <div className="text">Programs</div>
+<div className="content">
+<div className='icon'><img src={programcon} alt="" /></div> <div className="text">Programs</div></div>
+</NavLink>
+
+
+
+<NavLink to='/' className="list-item">
+<div className="content">
+<div className='icon'><img src={chatroomcon} alt="" /></div> <div className='chat'><span className="text">Chat Room</span><div className="chat-count">14</div></div></div>
 </NavLink>
 
 <NavLink to='/' className="list-item">
-<div className='icon'></div> <div className="text">Mentees</div>
+<div className="content">
+<div className='icon'><img src={calendacon} alt="" /></div> <div className="text">Calender</div></div>
 </NavLink>
 
 <NavLink to='/' className="list-item">
-<div className='icon'></div> <div className="text">Chat Room</div>
+<div className="content">
+<div className='icon'><img src={helpcon} alt="" /></div> <div className="text">Help Center</div></div>
 </NavLink>
 
 <NavLink to='/' className="list-item">
-<div className='icon'></div> <div className="text">Calender</div>
-</NavLink>
-
-<NavLink to='/' className="list-item">
-<div className='icon'></div> <div className="text">Help Center</div>
-</NavLink>
-
-<NavLink to='/' className="list-item">
-<div className='icon'></div> <div className="text">Settings</div>
+<div className="content">
+<div className='icon'><img src={settingscon} alt="" /></div> <div className="text">Settings</div></div>
 </NavLink> 
       </Menulist>
     )
--- src/components/main-display/side-bar/menulist/MenuList.styled.jsx
+++ src/components/main-display/side-bar/menulist/MenuList.styled.jsx
@@ -1,7 +1,63 @@
 import styled from "styled-components";
 
 export const Menulist = styled.div`
+width:80%;
+display:flex;
+flex-direction:column ;
+align-items:center;
+justify-content:space-evenly;
+height: 50%;
+.list-item{
+    text-decoration:none;
+    color:#02020C;
+    width:100%;
+    height:80%;
+    transition:0.3s all ease-in ;
+    &:hover{
+        background-color:#00a4bd8c ;
+        
+        &::before{
+    content:'';
+    height:100%;
+    width:6px;
+    background-color:#FFB300 ;
+    border-radius:20px ;
+    transition:0.3s all ease-in ;
+}
+    }
 
+    
+    display:flex;
+    justify-content:center;
+    align-items:center;
+.content{
+    width:100%;
+    display:flex;
+    align-items:center;
+    
+    .icon{
+        margin-left:20px ;
+        margin-right:20px ;
+    }
+    .chat{
+        display:flex;
+        .text{
+            margin-right:5px ;
+        }
+        .chat-count{
+            height: 20px;
+            width:20px;
+            background-color:#F52E2E;
+            border-radius:20px ;
+            color:white;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            font-size:10px ;
+        }
+    }
+}
+}
 
 
 
